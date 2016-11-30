3aceac9044b9f99bb0f465c0a10c43fe81f16174
joffrey <joffrey.p@compilatio.net>
2016-11-30 14:52:18 +0100

    Edit AbstractConfiguration method (get, exisit) to be generic and remove them from Memory.php

--- src/Core/AbstractConfiguration.php
+++ src/Core/AbstractConfiguration.php
@@ -9,9 +9,35 @@ abstract class AbstractConfiguration {
     protected $config = [];
     protected $manager;
     
-    public abstract function exist($name);
+    public function exist($name){
+        $levels = explode($this->manager->getNameDelimiter(), $name);
+        
+        $currentItem = $this->config;
+        foreach ($levels as $level) {
+            if(isset($currentItem[$level])){
+                $currentItem = $currentItem[$level];
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     
-    public abstract function get($name);
+    public function get($name){
+        $levels = explode($this->manager->getNameDelimiter(), $name);
+        
+        $currentItem = $this->config;
+        foreach ($levels as $level) {
+            if(isset($currentItem[$level])){
+                $currentItem = $currentItem[$level];
+            } else {
+                return null;
+            }
+        }
+        
+        return $currentItem;
+    }
     
     public final function setManager(Manager $manager){
         $this->manager = $manager;
--- src/Core/Memory.php
+++ src/Core/Memory.php
@@ -9,37 +9,7 @@ namespace JPC\Configuration\Core;
  */
 class Memory extends AbstractConfiguration {
     
-    function __construct($config = []) {
+   public function __construct($config = []) {
         $this->config = $config;
-    }
-
-    public function exist($name) {
-        $levels = explode($this->manager->getNameDelimiter(), $name);
-        
-        $currentItem = $this->config;
-        foreach ($levels as $level) {
-            if(isset($currentItem[$level])){
-                $currentItem = $currentItem[$level];
-            } else {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-
-    public function get($name) {
-        $levels = explode($this->manager->getNameDelimiter(), $name);
-        
-        $currentItem = $this->config;
-        foreach ($levels as $level) {
-            if(isset($currentItem[$level])){
-                $currentItem = $currentItem[$level];
-            } else {
-                return null;
-            }
-        }
-        
-        return $currentItem;
     }
 }
